1db3e7924e7c82b1b87bee2bca9470e890152548
Agustin Castiarena <castiarena@gmail.com>
2021-02-19 01:07:47 +0100

    aca agrgamos life cycle

--- src/FunctionComponent/Counter.js
+++ src/FunctionComponent/Counter.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Counter = ({ incrementText, incrementer = 1 }) => {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('mount');
+        return () => {
+            console.log('fc unmounted');
+        }
+    }, []);
+
+
+    useEffect(() => {
+        console.log(incrementText);
+    }, [incrementText]);
+    
     return (
         <div>
             <p>{count}</p>
-            <button onClick={() => setCount((prevState) => prevState + incrementer )}>
+            <button onClick={() => 
+                setCount((prevState) => prevState + incrementer )
+            }>
                 {incrementText}
             </button> 
         </div>
